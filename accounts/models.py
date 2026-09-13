813158ecbb934745ba193e88251e31f1480a47c7
Athika Priya <athikapriya1997@gmail.com>
2026-09-13 17:39:02 +0600

    chore : initialize project

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+# =========================================================
+# Custom user section
+# =========================================================
+class User(AbstractUser):
+    pass
